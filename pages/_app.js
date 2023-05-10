85ee22679882795c3b83d4d5297f884b642ecc74
anb1906423 <100342430+anb1906423@users.noreply.github.com>
2023-05-10 21:03:57 +0700

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Script from 'next/script';
 import reduxStore, { persistor } from '../redux';
 import { Provider } from 'react-redux';
+import Head from 'next/head';
 
 const SideBar = dynamic(() => import('../components/SideBar'), {
     ssr: false,
@@ -23,6 +24,10 @@ export default function MyApp({ Component, pageProps }) {
                     integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
                     crossOrigin="anonymous"
                 />
+                <Head>
+					<title>Administrator</title>
+					<link rel="icon" href="../favicon.png" />
+				</Head>
                 <Component {...pageProps} persistor={persistor} />
             </SideBar>
         </Provider>
